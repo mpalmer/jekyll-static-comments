4c27fb93d8d99cda3529c11e224410390d9af495
Matt Palmer <mpalmer@hezmatt.org>
2012-03-05 11:12:22 +1100

    Fix comment file glob so it actually finds files in subdirs

--- static_comments.rb
+++ static_comments.rb
@@ -42,7 +42,7 @@ module StaticComments
 	def self.read_comments(source)
 		comments = Hash.new() { |h, k| h[k] = Array.new }
 		
-		Dir["#{source}/**/_comments/**"].sort.each do |comment|
+		Dir["#{source}/**/_comments/**/*"].sort.each do |comment|
 			next unless File.file?(comment) and File.readable?(comment)
 			yaml_data = YAML::load_file(comment)
 			post_id = yaml_data.delete('post_id')
