8cdf0900c55c6b1a1e8f35adb37558f65cdcbc80
Christian Winkler <christian.winkler@gmail.com>
2015-11-12 20:32:18 +0100

    tabs

--- static_comments.rb
+++ static_comments.rb
@@ -19,42 +19,42 @@
 #  with this program; if not, see <http://www.gnu.org/licences/>
 
 Jekyll::Hooks.register :posts, :pre_render do |post, payload|
-  # code to call before Jekyll renders a post
-  id = payload["page"]["id"]
-  comments = StaticComments::find_for_post(payload["page"]["id"], payload["site"]["source"])
-  payload["page"]["comments"] = comments
-  payload["page"]["comment_count"] = comments.length
-  payload["page"]["has_comments"] = (comments.length > 0)
+	# code to call before Jekyll renders a post
+	id = payload["page"]["id"]
+	comments = StaticComments::find_for_post(payload["page"]["id"], payload["site"]["source"])
+	payload["page"]["comments"] = comments
+	payload["page"]["comment_count"] = comments.length
+	payload["page"]["has_comments"] = (comments.length > 0)
 end
 
 Jekyll::Hooks.register :site, :pre_render do |post, payload|
-  payload["site"]["posts"].each do |d|
-    comments = StaticComments::find_for_post(d.id, payload["site"]["source"])
-    
-    d.data["comment_count"] = comments.length
-  end
+	payload["site"]["posts"].each do |d|
+		comments = StaticComments::find_for_post(d.id, payload["site"]["source"])
+		
+		d.data["comment_count"] = comments.length
+	end
 end
 
 module StaticComments
-  # Find all the comments for a post
-  def self.find_for_post(post_id, site)
-    @comments ||= read_comments(site)
-    @comments[post_id]
-  end
+	# Find all the comments for a post
+	def self.find_for_post(post_id, site)
+		@comments ||= read_comments(site)
+		@comments[post_id]
+	end
 
-  # Read all the comments files in the site, and return them as a hash of
-  # arrays containing the comments, where the key to the array is the value
-  # of the 'post_id' field in the YAML data in the comments files.
-  def self.read_comments(source)
-    comments = Hash.new() { |h, k| h[k] = Array.new }
+	# Read all the comments files in the site, and return them as a hash of
+	# arrays containing the comments, where the key to the array is the value
+	# of the 'post_id' field in the YAML data in the comments files.
+	def self.read_comments(source)
+		comments = Hash.new() { |h, k| h[k] = Array.new }
 
-    Dir["#{source}/**/_comments/**/*"].sort.each do |comment|
-      next unless File.file?(comment) and File.readable?(comment)
-      yaml_data = YAML::load_file(comment)
-      post_id = yaml_data.delete('post_id')
-      comments[post_id] << yaml_data
-    end
+		Dir["#{source}/**/_comments/**/*"].sort.each do |comment|
+			next unless File.file?(comment) and File.readable?(comment)
+			yaml_data = YAML::load_file(comment)
+			post_id = yaml_data.delete('post_id')
+			comments[post_id] << yaml_data
+		end
 
-    comments
-  end
+		comments
+	end
 end
